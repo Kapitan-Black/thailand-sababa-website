64854efc12d72410f6774458bc4b92c24a2d5df3
thailandSababa <thailand.sababa.operations@gmail.com>
2024-10-23 18:49:17 +0700

    a

--- src/components/TopCard.tsx
+++ src/components/TopCard.tsx
@@ -9,19 +9,18 @@ type Props = {
 
 const TopCard = ({ image, text, textPosition = "bottom" }: Props) => {
   return (
-    
     <div
       dir="rtl"
-      className="relative bg-white shadow-lg rounded-xl overflow-hidden w-full flex-1"
+      className="relative bg-white shadow-lg rounded-xl overflow-hidden w-full flex-1 group"
     >
       <img
         src={image}
         alt="Card Image"
-        className="w-full h-[300px] md:h-[500px] object-cover rounded-xl"
+        className="w-full h-[300px] md:h-[500px] object-cover rounded-xl transform transition-transform duration-300 group-hover:scale-105"
       />
       <h3
         className={clsx(
-          "card absolute left-0 right-0 px-4 py-2 text-white text-lg text-center font-semibold bg-black bg-opacity-40",
+          "card absolute left-0 right-0 px-4 py-2 text-white text-lg text-center font-semibold bg-black bg-opacity-40 transform transition-transform duration-300 group-hover:scale-105",
           {
             "md:top-[15%] top-1/2 transform -translate-y-1/2":
               textPosition === "top",
